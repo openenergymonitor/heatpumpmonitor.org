12d48d939ef843ff421040c2a26de12db16554a9
TrystanLea <trystan.lea@gmail.com>
2022-11-13 17:07:04 +0000

    create directory for public web content

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # HeatpumpMonitor.org
 
 Source code for table of heat pumps with public dashboards
+
+Install public site content in /var/www
+
+    sudo ln -s /home/oem/heatpumpmonitor.org/www/ /var/www/hpmon
--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 SCRIPT=$(realpath "$0")
 cd $(dirname "$SCRIPT")
+echo $(dirname "$SCRIPT")
 
 URL='https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-eqigAmjwwSIc6snCYTWRYZW6wsVK98fsJ8kn4aiG_pDw8qgpc4y_ZkiHC_OtWpchDCk1nBwxza8W/pub?gid=447603213&single=true&output=tsv'
 
@@ -12,10 +13,13 @@ wget -q -nv $URL -O $TEMP
 
 if diff -q $FILE $TEMP > /dev/null
 then
+  echo "no change"
   # no change
   rm $TEMP
 else
   # new content
+  echo "new content"
   mv $TEMP $FILE
+  cp $FILE www/$FILE
 fi
 
