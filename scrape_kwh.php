218636a3fc776ccefb33301e37e50f38c544e141
MrTimbones <thefluffanutta@gmail.com>
2022-11-16 22:26:38 +0000

    Script to scrape values from emoncms sites

---
+++ scrape_kwh.php
@@ -0,0 +1,82 @@
+<?php
+  $input = fopen("sheet.tsv", "r");
+  $output = fopen("data.tsv", "w");
+  
+  $header = trim(fgets($input));
+  fputs($output, $header . "\tyear_elec\tyear_heat\n");
+  
+  # read reach row and append kWh readings for last year
+  while (($fields = fgetcsv($input, 1000, "\t")) !== FALSE) {
+    $kwh = scrapeEnergyValues($fields[10]);
+    $fields = array_merge($fields, $kwh);
+    fputs($output, implode("\t", $fields)."\n");
+  }
+  fclose($input);
+  fclose($output);
+
+  function scrapeEnergyValues($url)
+  {
+    print "$url\n";
+    
+    $fd = fopen($url, "r");
+    if (!$fd) {
+      print " - failed :(\n";
+      exit();
+    }
+    
+    $site = dirname($url);
+    
+    while (($line = fgets($fd)) !== false) {
+      if (preg_match('/^var apikey = "(.*)";/', $line, $matches)) {
+        $apikey = $matches[1];
+      }
+      if (preg_match('/^config.db = ({.*});/', $line, $matches)) {
+        $config = json_decode($matches[1]);
+      }
+    }
+    fclose($fd);
+    
+    if (!isset($config->heatpump_elec_kwh) || !isset($config->heatpump_heat_kwh)) {
+      return [ '-', '-' ];
+    }
+    
+    # fetch latest time and value for kWh feeds
+    $elec_data = fetchTimeValue($site, $config->heatpump_elec_kwh);
+    $heat_data = fetchTimeValue($site, $config->heatpump_heat_kwh);
+
+    # fetch values for 1 week ago  
+    #$weekago = $elec_data->time - 7 * 24 * 3600;
+    #$week_elec = fetchValue($site, $config->heatpump_elec_kwh, $weekago);
+    #$week_heat = fetchValue($site, $config->heatpump_heat_kwh, $weekago);
+    
+    # fetch values for a year ago
+    $yearago = $elec_data->time - 31536000;
+    $year_elec = fetchValue($site, $config->heatpump_elec_kwh, $yearago);
+    $year_heat = fetchValue($site, $config->heatpump_heat_kwh, $yearago);
+
+    # compute and return COP
+    return [ "elec" => round($elec_data->value - $year_elec),
+             "heat" => round($heat_data->value - $year_heat) ];
+  }
+
+  function fetchValue($site, $feed, $time)
+  {
+    $url = "$site/feed/value.json?id=%d&time=%d";
+    #if ($apikey != '') { $url .= "&apikey=$apikey"; }
+    $url = sprintf($url, $feed, $time);
+    $data = file_get_contents($url);
+    #print "$feed->$data\n";
+    return floatval($data);
+  }
+
+  function fetchTimeValue($site, $feed)
+  {
+    $url = "$site/feed/timevalue.json?id=%d";
+    #if ($apikey != '') { $url .= "&apikey=$apikey"; }
+    $url = sprintf($url, $feed);
+    $data = file_get_contents($url);
+    #print "$feed->$data\n";
+    return json_decode($data);
+  }
+
+?>
