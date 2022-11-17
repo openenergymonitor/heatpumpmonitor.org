812999131230594947c119050559516aacf85725
MrTimbones <thefluffanutta@gmail.com>
2022-11-17 15:38:46 +0000

    Check when emoncms data starts, and tweaked the table layout

--- scrape_kwh.php
+++ scrape_kwh.php
@@ -6,7 +6,7 @@
   
   # append new columns to header
   $header = trim(fgets($input));
-  fputs($output, $header . "\tyear_elec\tyear_heat\n");
+  fputs($output, $header . "\tyear_elec\tyear_heat\tsince\n");
   
   # read reach row and append kWh readings for last year
   while (($fields = fgetcsv($input, 1000, "\t")) !== FALSE) {
@@ -32,25 +32,35 @@
       return [ '-', '-' ];
     }
     
-    # fetch latest time and value for kWh feeds
-    $elec_data = fetchTimeValue($site, $config->heatpump_elec_kwh);
-    $heat_data = fetchTimeValue($site, $config->heatpump_heat_kwh);
+    # fetch meta data for kWh feeds
+    $elec_data = fetchFeedMeta($site, $config->heatpump_elec_kwh);
+    $heat_data = fetchFeedMeta($site, $config->heatpump_heat_kwh);
 
-    # fetch values for 1 week ago  
-    #$weekago = $elec_data->time - 7 * 24 * 3600;
-    #$week_elec = fetchValue($site, $config->heatpump_elec_kwh, $weekago);
-    #$week_heat = fetchValue($site, $config->heatpump_heat_kwh, $weekago);
+    # fetch last values from kWh feeds
+    $last_elec = fetchValue($site, $config->heatpump_elec_kwh, $elec_data->end_time);
+    $last_heat = fetchValue($site, $config->heatpump_heat_kwh, $heat_data->end_time);
+
+    # determine how far back to go
+    # either 1 year, start of feed or user configured start
+    $year_ago = $elec_data->end_time - 31536000;
+    $start_date = max($year_ago, $elec_data->start_time, $heat_data->start_time);
+    if (isset($config->start_date)) {
+      $start_date = max($start_date, $config->start_date);
+    }
     
-    # fetch values for a year ago
-    $yearago = $elec_data->time - 31536000;
-    $year_elec = fetchValue($site, $config->heatpump_elec_kwh, $yearago);
-    $year_heat = fetchValue($site, $config->heatpump_heat_kwh, $yearago);
+    # fetch values for a year ago (or since start_date)
+    $year_elec = fetchValue($site, $config->heatpump_elec_kwh, $start_date);
+    $year_heat = fetchValue($site, $config->heatpump_heat_kwh, $start_date);
 
-    # return kWh values
-    return [ "elec" => round($elec_data->value - $year_elec),
-             "heat" => round($heat_data->value - $year_heat) ];
+    # return kWh values and start date (0 means one whole year)
+    return [ "elec" => round($last_elec - $year_elec),
+             "heat" => round($last_heat - $year_heat),
+             "since" => $start_date > $year_ago ? $start_date : 0];
   }
 
+  /* atempts to get the app config from emoncms
+   * returns decoded json object
+   */
   function fetchConfig($url)
   {
     # attempt to pull the config for the app
@@ -67,18 +77,22 @@
     return false;
   }
   
+  /* fetch value from a feed at a specific unixtime 
+   * returns float
+   */
   function fetchValue($site, $feed, $time)
   {
-    $url = "$site/feed/value.json?id=%d&time=%d";
-    $url = sprintf($url, $feed, $time);
+    $url = sprintf("%s/feed/value.json?id=%d&time=%d", $site, $feed, $time);
     $data = file_get_contents($url);
     return floatval($data);
   }
-
-  function fetchTimeValue($site, $feed)
+  
+  /* fetch the meta data for a feed
+   * returns decoded json object
+   */
+  function fetchFeedMeta($site, $feed)
   {
-    $url = "$site/feed/timevalue.json?id=%d";
-    $url = sprintf($url, $feed);
+    $url = sprintf("%s/feed/getmeta.json?id=%d", $site, $feed);
     $data = file_get_contents($url);
     return json_decode($data);
   }
