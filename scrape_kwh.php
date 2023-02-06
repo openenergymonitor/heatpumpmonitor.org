6198934176fb06abaf958507ca7395a3668d594a
MrTimbones <thefluffanutta@gmail.com>
2023-02-06 15:02:06 +0000

    Override start date for one of the systems

--- scrape_kwh.php
+++ scrape_kwh.php
@@ -151,7 +151,7 @@
       }
       if (!isset($config->heatpump_elec_kwh)) { $config->heatpump_elec_kwh = 0; }
       if (!isset($config->heatpump_heat_kwh)) { $config->heatpump_heat_kwh = 0; }
-      if ($readkey=='4024d6f31ff9de4e30eb00744590bdc5') {
+      if (isset($readkey) && $readkey=='4024d6f31ff9de4e30eb00744590bdc5') {
         $config->start_date = 1674736200;
       }
       return $config;
