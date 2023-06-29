77ef077be2cf8f4f6d82b5175ed89154c85ed27a
TrystanLea <trystan.lea@gmail.com>
2023-06-29 14:51:43 +0100

    holding page for admin users

---
+++ www/Modules/user/admin_view.php
@@ -0,0 +1,21 @@
+<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+
+<div id="app" class="bg-light">
+    <div style=" background-color:#f0f0f0; padding-top:20px; padding-bottom:10px">
+        <div class="container" style="max-width:800px;">
+            <h3>Admin users</h3>
+        </div>
+    </div>
+    <div class="container" style="max-width:800px">
+        <br>
+        <div class="alert alert-warning" role="alert">
+            Not yet implemented
+        </div>
+        <br>
+        
+    </div>
+</div>
+
+<script>
+</script>
