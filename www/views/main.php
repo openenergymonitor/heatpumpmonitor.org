279da08074d354311c5bf6e48139d81ed6d87361
MrTimbones <thefluffanutta@gmail.com>
2023-07-04 21:21:54 +0000

    Tweaks to the table so it fits right

--- www/views/main.php
+++ www/views/main.php
@@ -17,14 +17,14 @@
       <tr>
         <th class="header">&nbsp;</td>
         <th class="header" colspan="6">Property</th>
-        <th class="header" colspan="4">Heating system</th>
+        <th class="header" colspan="3">Heating system</th>
         <th class="header" colspan="3">Annual Performance</th>
         <th class="header">&nbsp;</td>
       </tr>
       <tr>
         <th @click="sort('id', 'asc')" class="center">#</td>
         <!-- Property -->
-        <th @click="sort('location', 'asc')">Location</th>
+        <th @click="sort('location', 'asc')" class="lborder">Location</th>
         <th @click="sort('property', 'asc')">Type</th>
         <th @click="sort('age', 'desc')">Built</th>
         <th @click="sort('floor_area', 'desc')" class="right">Floor Area</th>
@@ -32,16 +32,16 @@
         <th @click="sort('insulation', 'asc')">Insulation</th>
         
         <!-- Heating System -->
-        <th @click="sort('hp_model', 'asc')" class="border">Make / Model</th>
+        <th @click="sort('hp_model', 'asc')" class="lborder">Make / Model</th>
         <th @click="sort('hp_output', 'desc')" class="right">Output</th>
         <th @click="sort('hp_type', 'asc')">Source</th>
         <!--<th @click="sort('emitters', 'asc')">Emitters</th>-->
         
         <!-- Performance -->
-        <th @click="sort('year_elec', 'desc')" class="border right">Electric</th>
+        <th @click="sort('year_elec', 'desc')" class="lborder right">Electric</th>
         <th @click="sort('year_heat', 'desc')" class="right">Heat</th>
         <th @click="sort('year_cop', 'desc')" class="center">SCOP</th>
-        <th class="nosort border">Charts</th>
+        <th class="nosort lborder">Charts</th>
       </tr>
     </thead>
     
@@ -54,8 +54,7 @@
         <td class="nowrap">{{n.age.replace(' to ', '-').trim()}}</td>
         <td class="nowrap" align="right">{{unit_dp(n.floor_area, 'm&sup2;')}}</td>
         <td class="nowrap" align="right">{{unit_dp(n.heat_demand, 'kWh')}}</td>
-        <td v-bind:title="n.insulation">{{n.insulation.split(' ').slice(0, 2).join(' ')}}
-           {{n.insulation.split(' ').length > 2 ? '&mldr;' : ''}}</td>
+        <td v-bind:title="n.insulation">{{n.insulation.split(' ').slice(0, 2).join(' ')}}</td>
            
         <!-- Heating System -->
         <td v-bind:title="n.hp_model">{{n.hp_model.replace(/ \(.*\)/, ' &mldr;')}}</td>
@@ -79,7 +78,7 @@
       
       <tr class="extra" style="display: none">
         <td class="extra">&nbsp;</td>
-        <td colspan="14" class="extra">
+        <td colspan="13" class="extra">
           <b v-if="n.refrigerant">Refrigerant:</b> {{n.refrigerant}}
           <b v-if="n.flow_temp">Flow Temp:</b> {{n.flow_temp}}
           <b v-if="n.buffer">Buffer:</b> {{n.buffer}}
@@ -96,9 +95,9 @@
     <tfoot>
       <td colspan="1" class="footer"></td>
       <td colspan="6" class="footer">Recently added systems denoted by &#10024;</td>
-      <td colspan="4" class="footer"></td>
+      <td colspan="3" class="footer"></td>
       <td colspan="3" class="footer"><i>Incomplete data in grey</i></td>
-      <td colspan="2" class="footer"></td>
+      <td colspan="1" class="footer"></td>
     </tfoot>
   </table>
   
