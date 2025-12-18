67534b08a41e8a9d1ded75077e23d4eb14bc854a
Trystan Lea <trystan.lea@gmail.com>
2025-12-18 21:48:37 +0000

    updated icons

--- www/theme/theme.php
+++ www/theme/theme.php
@@ -25,11 +25,11 @@ $navigation = array(
     <meta name="theme-color" content="#44b3e2">
     
     <!-- Icons -->
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $path; ?>theme/img/logo/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $path; ?>theme/img/logo/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $path; ?>theme/img/logo/apple-touch-icon.png">
     <link rel="manifest" href="/site.webmanifest">
-    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo $path; ?>theme/img/logo/favicon.ico">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
@@ -49,8 +49,8 @@ $navigation = array(
     <link rel="manifest" href="<?php echo $path; ?>manifest.json">
     
     <!-- Icons -->
-    <link rel="icon" type="image/x-icon" href="<?php echo $path; ?>theme/img/icons/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php echo $path; ?>theme/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/x-icon" href="<?php echo $path; ?>theme/img/logo/favicon.ico">
+    <link rel="apple-touch-icon" href="<?php echo $path; ?>theme/img/logo/apple-touch-icon.png">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css">
